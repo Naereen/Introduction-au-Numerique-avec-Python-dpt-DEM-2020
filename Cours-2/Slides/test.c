bbc94057761980c79b2dfa5f7ba405354b8a50a0
Lilian Besson <lilian@besson.link>
2020-10-07 21:48:54 +0200

    Ajout programme C et assembleur, que j'avais écrit pour les curieux-ses pour le CM#2

---
+++ Cours-2/Slides/test.c
@@ -0,0 +1,8 @@
+/* Test de compilation en binaire et en assembleur */
+#include "stdio.h"
+
+int main() {
+    int x = 3;
+    int y = 20;
+    printf("%i\n", x + y);
+}
---
+++ Cours-2/Slides/test.s
@@ -0,0 +1,35 @@
+	.file	"test.c"
+	.text
+	.section	.rodata
+.LC0:
+	.string	"%i\n"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movl	$3, -8(%rbp)
+	movl	$20, -4(%rbp)
+	movl	-8(%rbp), %edx
+	movl	-4(%rbp), %eax
+	addl	%edx, %eax
+	movl	%eax, %esi
+	leaq	.LC0(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	movl	$0, %eax
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 7.5.0-3ubuntu1~18.04) 7.5.0"
+	.section	.note.GNU-stack,"",@progbits
