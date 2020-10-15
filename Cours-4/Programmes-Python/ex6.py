154b4434cbf42f35e430be210779e1f789b6e3e5
Lilian Besson <lilian@besson.link>
2020-10-15 14:06:07 +0200

    Mise à jour Evaluation.tex

--- Cours-4/Programmes-Python/ex6.py
+++ Cours-4/Programmes-Python/ex6.py
@@ -4,11 +4,11 @@ def ecart_type(maliste):
     moy = sum(maliste) / len(maliste)
     i = 0
     somme = 0  # commence à 0
-    while i < len(maliste:
+    while i < len(maliste):
         somme = somme + (maliste[i] - moy)**2
         i = i + 1
     return sqrt(somme / len(maliste))
 
 maliste = [1, 2, 3, 4, 5, 6]
-son_stddev = ecart_type(maliste)  # 3.5
+son_stddev = ecart_type(maliste)  # ~= 1.7078...
 print("L'écart-type des éléments de ma liste est", son_stddev)
